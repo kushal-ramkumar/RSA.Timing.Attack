4e4f93cfafe8cf9be701c2152906ad48a8bfa491
kushal-ramkumar <kushal.ramkumar@gmail.com>
2017-11-16 06:31:45 +0530

    Implemented Montgomery Reduction, nPrime, RSA

--- RSATimingAttack.py
+++ RSATimingAttack.py
@@ -1,3 +1,5 @@
+import math
+
 def ModInverse(a, n):
     (t, t_new, r, r_new) = (0, 1, int(n), int(a))
 
@@ -14,6 +16,49 @@ def ModInverse(a, n):
         
     return t
 
+def MontgomeryProduct (a, b, n, nPrime, r):
+    t = a * b
+    m = t * nPrime % r
+    u = (t + m*n)/r
+
+    if u >= n:
+        return (u-n, True)
+    else:
+        return (u, False)
+
+def nPrime(n):
+    k = math.floor(math.log(int(n), 2)) + 1
+    r = int(math.pow(2,k))
+    rInverse = ModInverse(r,n)
+    nPrime = (r * rInverse - 1)//n 
+    return (r, nPrime)
+
+def rsa(a, d, n, nPrime, r):
+    aBar = (a*r)%n
+    xBar = (1*r)%n
+    k = len(d)
+    sub_count = 0
+    for i in range(0, k):
+        sub = False
+        xBar, tmp = MontgomeryProduct(xBar, xBar, n, nPrime, r)
+        if d[i] == '1':
+            xBar, sub = MontgomeryProduct(aBar, xBar, n, nPrime, r)
+        sub_count += int(sub)
+    x, tmp = MontgomeryProduct(xBar, 1, n, nPrime, r)
+    return x, sub_count 
+
 if __name__ == "__main__":
-    print(ModInverse(3, 11))
-    print(ModInverse(10, 17))
+    message = 10113892728
+    print "Message : " + str(message)
+    #d = '111111010000101111101001101011111'
+    d = "{0:b}".format(8490832735)
+    n = int(10967535067)
+    #e = '11111'
+    e = "{0:b}".format(31)
+    r, nPrime = nPrime(n)
+    encMessage, tmp = rsa(message, str(e), n, nPrime, r)
+    decMessage, tmp = rsa(encMessage, str(d), n, nPrime, r)
+    if decMessage == message:
+        print "Decryption Successful " + str(decMessage)
+    else:
+        print "Decryption Failed!"
